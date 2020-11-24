19450416b7ebba287867e2006edfdb69a5093a38
nbrajic <brajic.nevena@gmail.com>
2020-11-24 23:32:52 +0100

    org

--- src/pages/SingleOrderAdd/SingleOrderAdd.jsx
+++ src/pages/SingleOrderAdd/SingleOrderAdd.jsx
@@ -3,9 +3,6 @@ import { useParams } from "react-router-dom";
 import NavBar from "../../components/NavBar";
 import { addOrderItem, getMeals, getOneOrder, getProfile } from "../../services/services";
 import "./singleOrderAdd.css";
-// import { formatDate } from "../../utilities/utilities";
-// import RestaurantItem from "../../components/RestaurantItem";
-
 
 const SingleOrderAdd = () => {
 
@@ -15,48 +12,47 @@ const SingleOrderAdd = () => {
   const [payload,setPayload] = useState([]);
   const [payloadItem, setPayloadItem] = useState({ quantity: 0, mealId: "", note: "" });
   const [orderedMeal,setOrderedMeal] = useState([]);
-  // const [quantity,setQuantity] = useState(0)
   const { slug } = useParams();
-  // const pollId = "001a1b51-6c18-47ca-a2c5-81a3c8d354ef";
-  // const [restaurantId,setRestaurantId] = useState(undefined)
   const restaurantId = order && order.restaurantId;
   const [valid,setValid] = useState(true);
   const [total,setTotal] = useState(0);
 
-
-  //Hvatam jedan order
   useEffect(() => {
+
     getOneOrder(slug)
       .then((res) => {
         const data = res.data;
         setOrder(data);
         console.log(res.data);
-        // setRestaurantId(order && order.restaurantId)
       })
       .catch((err) => {
         console.log(err);
       });
 
-    restaurantId &&
-      getMeals(restaurantId).then((res) => {
-        console.log(res.data);
-        console.log(res.data.filter((el) => el.available));
-        setMeals(res.data.filter((el) => el.available));
-      });
+      restaurantId &&
+        getMeals(restaurantId).then((res) => {
+          console.log(res.data.filter((el) => el.available));
+          setMeals(res.data.filter((el) => el.available));
+        });
+
   }, [slug, restaurantId]);
 
   useEffect(() => {
+
     getProfile().then((res) => {
       console.log(res);
       setProfile(res.data);
     });
+    
   }, []);
 
   const deleteItem = (name, quantity, note, price) => {
-    let index = orderedMeal.findIndex(el => el.name === name && el.note === note && el.quantity === quantity);
+    let index = orderedMeal.findIndex(
+      el => el.name === name 
+            && el.note === note 
+            && el.quantity === quantity);
     orderedMeal.splice(index, 1);
     setTotal(prev => prev - price * quantity);
-
   };
 
   const handlesValidation = () => {
@@ -88,31 +84,30 @@ const SingleOrderAdd = () => {
   const addItem = (mealId,mealName,mealPrice) => {
    
     if (payloadItem.quantity >= 1) {
-      //to check if input is valid
       payloadItem.quantity = Number(payloadItem.quantity);
       payloadItem.mealId = mealId;
-      setPayload(prev=>{
+      setPayload(prev => {
         return [
           ...prev,
           payloadItem
-      ]
-      })
-
+        ]
+      });
     }
 
     else setPayload(prev=>{
       return[
         ...prev
       ]
-    })
+    });
     
     setOrderedMeal(prev=>{
       if(payloadItem.quantity >= 1) {
       return [
         ...prev,
         {name:mealName,price:mealPrice,quantity:payloadItem.quantity,note:payloadItem.note}
-      ]
+      ];
     }
+
     else {
       return [...prev]
     }})
@@ -122,27 +117,19 @@ const SingleOrderAdd = () => {
   };
 
   const addItemsToOrder = () => {
-      // Data example:
-  // let data = {
-  //  "consumer": "Veljko",
-  //  "payloads": [
-  //    {
-  //      "quantity": 1,
-  //      "mealId": "d53e202a-317c-4bad-99d5-1b7a446f9e26",
-  //      "note": "bez luka" 
-  //    }
-  //   ]
-  // }
   const data = {
     consumer: profile.firstName + ' ' + profile.lastName,
     payloads:payload
-  }
+  };
+
   if(order.active === true) {
-   addOrderItem(data,slug).then(res=>{
+
+   addOrderItem(data,slug).then(res => {
      console.log(res)
    })}
+
    else handlesValidation()
-  }
+  };
 
   return (
     <div id="single-order-create">
